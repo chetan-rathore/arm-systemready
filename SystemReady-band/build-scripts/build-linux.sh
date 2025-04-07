b94e407800db4f1363dd1e4788480e33256ec10a
chetan singh rathore <chetanrgithub@gmail.com>
2025-04-07 22:46:13 +0530

    Update build-linux.sh : Add configs for USB_PCI in 6.12

--- SystemReady-band/build-scripts/build-linux.sh
+++ SystemReady-band/build-scripts/build-linux.sh
@@ -85,6 +85,9 @@ do_build ()
     #Configurations to increase serial ports
     echo "CONFIG_SERIAL_8250_NR_UARTS=32" >> $LINUX_OUT_DIR/.config
     echo "CONFIG_SERIAL_8250_RUNTIME_UARTS=32" >> $LINUX_OUT_DIR/.config
+    # 6.12 linux kernel was missing below USB configs, causing issues.
+    echo "CONFIG_USB_PCI=y" >> $LINUX_OUT_DIR/.config
+    echo "CONFIG_USB_XHCI_PCI=m" >> $LINUX_OUT_DIR/.config
 
     if [[ $arch = "aarch64" ]]
     then
